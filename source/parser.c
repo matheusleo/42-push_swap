5e0eaabf7f24df77628ae6d708e0843819a363c1
matheus.leonardo <matheusleotrab@gmail.com>
2023-02-19 17:11:30 -0300

    mandatory - apply the indexer
    apply the indexer function to the parser after setting it up and checking for meory leaks. PS.: handle the unitialised variables

--- source/parser.c
+++ source/parser.c
@@ -6,13 +6,13 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 20:45:55 by mleonard          #+#    #+#             */
-/*   Updated: 2023/02/17 00:48:25 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/02/19 17:09:38 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-static t_stack_el	*create_new_stack_el(char *nptr, t_push_swap *push_swap)
+static t_stack_el	*create_stack_el(char *nptr, t_push_swap *push_swap)
 {
 	t_stack_el	*temp;
 	long int	nb_temp;
@@ -37,23 +37,30 @@ static void	set_pos(t_stack_el *stack_el)
 	pos++;
 }
 
-void	parse_input(int argc, char *argv[], t_push_swap *push_swap)
+static t_push_swap	*create_stack(int argc, char **argv, t_push_swap *push_swap)
 {
 	int			idx;
 	t_stack_el	*temp_nb;
 	t_list		*new_node;
 
 	idx = 1;
-	temp_nb = create_new_stack_el(argv[idx], push_swap);
+	temp_nb = create_stack_el(argv[idx], push_swap);
 	push_swap->stack_a = ft_lstnew(temp_nb);
 	idx++;
 	while (idx < argc)
 	{
-		temp_nb = create_new_stack_el(argv[idx], push_swap);
+		temp_nb = create_stack_el(argv[idx], push_swap);
 		new_node = ft_lstnew(temp_nb);
 		ft_lstadd_back(&push_swap->stack_a, new_node);
 		idx++;
 	}
+	return (push_swap);
+}
+
+void	parse_input(int argc, char *argv[], t_push_swap *push_swap)
+{
+	create_stack(argc, argv, push_swap);
 	ft_lstiter(push_swap->stack_a, set_pos);
 	validate_doubles(push_swap);
+	set_indexes(&push_swap->stack_a);
 }
