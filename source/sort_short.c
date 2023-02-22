2f7edf4c319f9ca8c839e2a41a31bfcf42d9f915
matheus.leonardo <matheusleotrab@gmail.com>
2023-02-22 09:41:30 -0300

    mandatory - fix sort short
    fix sort_short function which was throwing a segmentation fault

--- source/sort_short.c
+++ source/sort_short.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:23:04 by mleonard          #+#    #+#             */
-/*   Updated: 2023/02/22 09:10:20 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/02/22 09:40:24 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@ void	sort_short(t_push_swap *push_swap)
 
 	stack_a = push_swap->stack_a;
 	if (ft_lstsize(stack_a) == 2)
-		return (swap_a(&stack_a));
+		return (swap_a(&push_swap->stack_a));
 	else
 	{
 		while (!is_stack_sorted(stack_a))
 		{
 			stack_a_content = stack_a->content;
-			if ((stack_a_content->index == 3))
-				rotate_a(&stack_a);
-			else if ((stack_a_content->index == 1))
-				reverse_a(&stack_a);
-			else if ((stack_a_content->index == 2))
+			if (stack_a_content->index == 3)
+				rotate_a(&push_swap->stack_a);
+			else if (stack_a_content->index == 1)
+				reverse_a(&push_swap->stack_a);
+			else if (stack_a_content->index == 2)
 			{
 				if (((t_stack_el *)stack_a->next->content)->index == 1)
-					swap_a(&stack_a);
+					swap_a(&push_swap->stack_a);
 				else
-					reverse_a(&stack_a);
+					reverse_a(&push_swap->stack_a);
 			}
 			stack_a = push_swap->stack_a;
 		}
