e47d62044f729af9de5d11f6a7ab492384a52afc
matheus.leonardo <matheusleotrab@gmail.com>
2023-02-22 09:12:43 -0300

    mandatory - implement sorting algorithm pt.2
    implement the function responsible for sorting the short stack

--- source/sort_short.c
+++ source/sort_short.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:23:04 by mleonard          #+#    #+#             */
-/*   Updated: 2023/02/22 09:10:09 by mleonard         ###   ########.fr       */
+/*   Updated: 2023/02/22 09:10:20 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 void	sort_short(t_push_swap *push_swap)
 {
-	ft_printf("from sort short\n");
+	t_list		*stack_a;
+	t_stack_el	*stack_a_content;
+
+	stack_a = push_swap->stack_a;
+	if (ft_lstsize(stack_a) == 2)
+		return (swap_a(&stack_a));
+	else
+	{
+		while (!is_stack_sorted(stack_a))
+		{
+			stack_a_content = stack_a->content;
+			if ((stack_a_content->index == 3))
+				rotate_a(&stack_a);
+			else if ((stack_a_content->index == 1))
+				reverse_a(&stack_a);
+			else if ((stack_a_content->index == 2))
+			{
+				if (((t_stack_el *)stack_a->next->content)->index == 1)
+					swap_a(&stack_a);
+				else
+					reverse_a(&stack_a);
+			}
+			stack_a = push_swap->stack_a;
+		}
+	}
 }
